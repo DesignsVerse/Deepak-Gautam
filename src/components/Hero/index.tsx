13e35ae47971fe31611a4384c0cbeaa2ad02e379
harshmalviya2003 <harshmalviya343@gmail.com>
2025-02-20 11:28:18 +0530

    #done

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
           {/* 📸 Right Side - Astrologer Image */}
           <div className="relative mb-14 flex flex-col items-center md:order-2 z-10">
             <motion.div
-              className="absolute w-64 h-64 md:w-[380px] md:h-[380px] rounded-full bg-cover bg-center top-1.5"
+              className="absolute w-60 h-60 md:w-[380px] md:h-[380px] rounded-full bg-cover bg-center top-1.5"
               style={{ backgroundImage: "url('/bg.webp')" }}
               animate={{ rotate: [0, 360] }}
               transition={{ repeat: Infinity, duration: 60, ease: "linear" }}
