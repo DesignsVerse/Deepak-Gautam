8f99dbf24bdcea6e527cec50906fbbd9c7c3deca
DesignsVerse <designsverseagency@gmail.com>
2025-05-19 14:11:52 +0530

    Update index.tsx

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -27,7 +27,7 @@ const Banner: React.FC = () => {
   };
 
   return (
-    <div className=" relative mt-[px] w-full h-[70vh] overflow-hidden z-10">
+    <div className=" relative mt-[px] w-full h-full overflow-hidden z-10">
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index} className="relative w-full h-full">
